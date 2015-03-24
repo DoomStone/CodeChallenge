3f28d6fedcf76d16b9876404e3364740633a542e
krs <krs@tradeshift.com>
2015-03-24 12:01:34 +0100

    A little clean up

--- codechallenge/src/main/java/com/tradeshift/ApplicationContext.java
+++ codechallenge/src/main/java/com/tradeshift/ApplicationContext.java
@@ -3,7 +3,6 @@ package com.tradeshift;
 import com.tradeshift.service.EnglishHelloService;
 import com.tradeshift.service.HelloService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
